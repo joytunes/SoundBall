25add3266274255821b88c1954306fea0e8d6bd5
Noam Gat <noamgat@gmail.com>
2012-09-06 23:19:13 +0300

    Started adding FFT logic

--- Assets/Scripts/FFTController.cs
+++ Assets/Scripts/FFTController.cs
@@ -3,20 +3,37 @@ using System.Collections;
 
 public class FFTController : MonoBehaviour 
 {
+    public int numSamples = 128;
+    public GameObject spectrumBallTemplate;
 
     private float[] spectrumSamples;
+    private GameObject[] spectrumBalls;
+
 	// Use this for initialization
 	void Start () {
         Debug.Log("Devices : " + string.Join(",", Microphone.devices));
-        audio.clip = Microphone.Start(null, true, 100, 44100);
-        spectrumSamples = new float[128];
+        //audio.clip = Microphone.Start(null, true, 5, 44100);
+        spectrumSamples = new float[numSamples];
+        spectrumBalls = new GameObject[numSamples];
+        for (int i = 0; i < numSamples; i++)
+        {
+            spectrumBalls[i] = (GameObject)GameObject.Instantiate(spectrumBallTemplate);
+            spectrumBalls[i].transform.parent = this.transform;
+            spectrumBalls[i].transform.localPosition = new Vector3(i, 0, 0);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        audio.GetSpectrumData(spectrumSamples, 0, FFTWindow.Rectangular);
+        for (int i = 0; i < numSamples; i++)
+        {
+            spectrumBalls[i].transform.localPosition = new Vector3(i, spectrumSamples[i], 0);
+        }
+
         if (Time.frameCount % 60 == 0)
         {
-            audio.GetSpectrumData(spectrumSamples, 0, FFTWindow.Rectangular);
             string[] floatStrings = System.Array.ConvertAll<float, System.String>(spectrumSamples, delegate (float input) { return input.ToString(); });
             Debug.Log(string.Join(",", floatStrings));
         }
