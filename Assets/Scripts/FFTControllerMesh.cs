a34cd272bdeb7e148811a30e936f25fa14b64bb6
Yuval Kaminka <yuval.kaminka@joytunes.com>
2012-09-07 01:47:29 +0300

    box width

--- Assets/Scripts/FFTControllerMesh.cs
+++ Assets/Scripts/FFTControllerMesh.cs
@@ -5,10 +5,10 @@ public class FFTControllerMesh : MonoBehaviour {
 
     public FFTController sourceController;
     public MeshFilter targetMesh;
-
+    public int boxWidth = 10;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
@@ -27,8 +27,12 @@ public class FFTControllerMesh : MonoBehaviour {
 
         for (int i = 0; i < samples.Length ; i++)
         {
-            triangleVertices[2 * i] = new Vector3(i, 0, 0);
+            triangleVertices[2 * i] = new Vector3(i, -boxWidth, 0);
             triangleVertices[2 * i + 1] = new Vector3(i, samples[i], 0);
+        }
+
+        for (int i = 0; i < (samples.Length-1) * 2; i++)
+        {
             triangleIndices[3 * i] = i;
             triangleIndices[3 * i + 1] = i + 1;
             triangleIndices[3 * i + 2] = i + 2;
@@ -39,7 +43,7 @@ public class FFTControllerMesh : MonoBehaviour {
 
         if (Time.frameCount % 60 == 0)
         {
-            string[] intStrings = System.Array.ConvertAll<Vector3, System.String>(samplesMesh.vertices, delegate(Vector3 input) { return input.ToString(); });
+            string[] intStrings = System.Array.ConvertAll<int, System.String>(samplesMesh.triangles, delegate(int input) { return input.ToString(); });
             //Debug.Log("triangles:" + string.Join(",", intStrings));
             //Debug.Log("samples len:" + samples.Length.ToString());
         }
