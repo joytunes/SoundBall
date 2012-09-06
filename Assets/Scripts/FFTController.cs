16959c2fb175d1596437d5eea57ef27981a95b11
Noam Gat <noamgat@gmail.com>
2012-09-06 22:44:09 +0300

    First project skeleton

---
+++ Assets/Scripts/FFTController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class FFTController : MonoBehaviour 
+{
+
+    private float[] spectrumSamples;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Devices : " + string.Join(",", Microphone.devices));
+        audio.clip = Microphone.Start(null, true, 100, 44100);
+        spectrumSamples = new float[128];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.frameCount % 60 == 0)
+        {
+            audio.GetSpectrumData(spectrumSamples, 0, FFTWindow.Rectangular);
+            string[] floatStrings = System.Array.ConvertAll<float, System.String>(spectrumSamples, delegate (float input) { return input.ToString(); });
+            Debug.Log(string.Join(",", floatStrings));
+        }
+	}
+}
